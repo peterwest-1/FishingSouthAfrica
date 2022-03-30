e365291d118d2c9d6258096ef2ed6053415deb5c
Pete West <peterwest86@gmail.com>
2022-03-30 12:37:34 +0200

    Initial Commit

---
+++ FishingSouthAfrica/FishingSouthAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingSouthAfricaApp.swift
+//  FishingSouthAfrica
+//
+//  Created by Pete West on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct FishingSouthAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
